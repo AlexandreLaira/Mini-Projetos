c362116ee0c88a364c38a01945379e7aa2e6d523
Alexandre Laira <alexandrelaira2@gmail.com>
2023-05-19 09:57:28 -0300

    adicionando mousemove

--- Parallax/script.js
+++ Parallax/script.js
@@ -1,20 +1,31 @@
  
 
 let text = document.getElementById('text');
-let dragon = document.getElementById('dragon');
+let dragon = document.querySelector('#dragon');
 let folhas = document.getElementById('folhas');
 let folhas2 = document.getElementById('folhas2');
 let smoke = document.getElementById('smoke');
 
 
-window.addEventListener('mousemove', ()=>{
-  let value = window.scrollY;
-  text.style.marginTop = value * 2.5 + 'px';
-  text.style.color = `rgba(${value}, ${value * 2.5}, ${value * 4}, 1)`;
-  dragon.style.left = value * -1.5 + 'px';
-  folhas2.style.left = value * -1.2 + 'px';
-  folhas.style.left = value * 1.2 + 'px';
-  smoke.style.opacity = 1 - (value / 500);
+function scroll(){
+  let valor = window.scrollY;
+  dragon.style.left = valor * -1.23 + 'px';
+  text.style.marginTop = valor * 2.33 + 'px';
+  text.style.color = `rgba(${valor}, ${valor * 2.5}, ${valor * 4}, 1)`;
+  smoke.style.opacity = 1 - (valor / 500);
+}
 
+function mouse(e, elemento){
+  let mouseX = e.pageX * 4.3;
+  let mouseY = e.pageY * 4.3;
+
+  elemento.style.transform = `translate(${mouseX/100}px , ${mouseY/100}px)`;
+}
+
+
+window.addEventListener('scroll', scroll);
+
+window.addEventListener('mousemove', (event)=>{
+  mouse(event, folhas);
 });
 
